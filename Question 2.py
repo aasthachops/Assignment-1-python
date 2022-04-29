be7681c66066b0a8c990adafb6c236ad31a922db
aasthachops <104620228+aasthachops@users.noreply.github.com>
2022-04-29 14:42:42 +0530

    Delete Question 2.py

--- Question 2.py
+++
@@ -1,8 +0,0 @@
-a= float(input("Enter the Gross Income ",))
-b= int(input("Enter the Number of Dependents ",))
-taxRate= .20
-standardDeduction = 10000
-dependentDeduction= 3000
-taxableIncome= a-standardDeduction-(b*dependentDeduction)
-tax= taxableIncome*taxRate
-print("The Tax is ",tax)
